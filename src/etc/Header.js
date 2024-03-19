cc279900a17fc33de99f9e4716d2b8c81228046d
so0y <jenny1717@naver.com>
2024-03-19 11:39:18 +0900

    Header Edit

--- src/etc/Header.js
+++ src/etc/Header.js
@@ -1,6 +1,33 @@
+import TopHeader from "./TopHeader";
+import './css/Header.css'
+import { NavLink } from "react-router-dom";
+
 function Header() {
     return (
-        <div>헤더 입니다.</div>
+        <div className="Header">
+            <div className='MainLogo'>
+                <NavLink to="/main">
+                <img src={process.env.PUBLIC_URL + '/img/LOGO.png'} width='200px' alt='Logo' className='Logo'/>
+                </NavLink>
+            </div>
+            <div className="Headers">
+                <TopHeader></TopHeader>
+                <ul className='Menu'>
+                        <li className="Menu_Box">
+                            <NavLink to="/search" activeClassName="active">맛집 검색</NavLink>
+                        </li>
+                        <li className="Menu_Box">
+                            <NavLink to="/review" activeClassName="active">맛집 리뷰</NavLink>
+                        </li>
+                        <li className="Menu_Box">
+                            <NavLink to="/recipe"activeClassName="active">레시피</NavLink>
+                        </li>
+                        <li className="Menu_Box">
+                            <NavLink to="/lanking" activeClassName="active">랭킹</NavLink>
+                        </li>
+                    </ul>
+            </div>
+        </div>
     );
 }
 
